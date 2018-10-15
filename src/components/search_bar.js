cccf49d53deb4d553e16ee6fdaa33d117a407a99
emmeiwhite <emmeiwhite@gmail.com>
2018-10-15 18:49:17 +0530

    Handling events is at the core of any library or framework

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 class SearchBar extends React.Component{
     render(){
-        return <input />;
+        return <input onChange={event=> console.log(event.target.value)}/>;
     }
-   
 }
 
 export default SearchBar;
