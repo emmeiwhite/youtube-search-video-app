89911178446cbe34fbe78a0bad4dc16eeb33fed7
emmeiwhite <emmeiwhite@gmail.com>
2018-10-15 18:23:34 +0530

    Class Based Components being used

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+// Here is the react component, probably the Root Componet in this case.
+// We always create one Component per file... No matter how small the Component!!!
+
+// YOUTUBE API KEY: AIzaSyAGJmD1kXSd5CYpWNP88fn61HokH-1IpSk
+// Package Needed:  " youtube-api-search "
+
+
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = ()=>{
-    return <div>HELLO GRIDER !!! How is Life , han</div>
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAGJmD1kXSd5CYpWNP88fn61HokH-1IpSk';
+
+class App extends Component{
+    render(){
+        return <SearchBar onChange={this.changeValue}/>
+    }    
 }
 
 
