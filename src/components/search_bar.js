8dcd575703589481dd41066a76065df80fe0eef9
emmeiwhite <emmeiwhite@gmail.com>
2018-10-15 20:36:34 +0530

    props being passed successfully from App to VideoList

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+
+        // state is an object which holds the data of our component
+        this.state = {
+            term:''
+        }
+    }
     render(){
-        return <input onChange={event=> console.log(event.target.value)}/>;
+        // Controlled Component : I guess better term is Controlled Input in this case.
+        // I learned that Controlled Component is one, which takes its data from the parent 
+        // and doesn't have it's own state. Mosh taught that... Now Stephen Grider is teaching 
+        // 'A component in which the input value depends on the state' is Controlled Component.
+        // Both are great Mentors, I however go with Mosh Hamedani's Definition !!!
+        return (
+            <div>
+                <input 
+                    value={this.state.term} 
+                    onChange={event=>this.setState({term:event.target.value})}/>
+                Current Value is : {this.state.term}
+            </div>
+        ) 
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -7,14 +7,38 @@
 
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar';
+import SearchBar from './components/search_bar'; 
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAGJmD1kXSd5CYpWNP88fn61HokH-1IpSk';
 
+
+
 class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos:[] // Initially there wouldn't be any call made to the API
+        }
+
+        // For now we making the API call here in the constructor only, but it's not a good practice
+        YTSearch({key:API_KEY,term:'Lenovo Laptops 8th Generation'},(videos)=>{ //data came in the form of array itself :)
+            this.setState({videos})
+            console.log(videos)
+        })
+    }
+
     render(){
-        return <SearchBar onChange={this.changeValue}/>
+        return(
+            <React.Fragment>
+                <SearchBar onChange={this.changeValue}/>
+                <VideoList videos={this.state.videos}/>
+            </React.Fragment>
+        ) 
+      
     }    
 }
 
