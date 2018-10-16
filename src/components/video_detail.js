e2279186986ae42ccdebda9ff74c39122cfeaab7
emmeiwhite <emmeiwhite@gmail.com>
2018-10-16 15:58:13 +0530

    Know that it takes time to fetch the data Asynchronously from API

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const VideoDetail = ({video})=>{
+    // In every Asynchronous call 
+    if(!video){
+        // While Data has not been served by the API, we render this JSX
+        return <h1>"Loading ..."</h1> 
+    }
+    // console.log(video);
+    const videoId = video.id.videoId;
+    const youtube_url = `https://youtube.com/embed/${videoId}`
+
     return(
-        <div>Hello World!!</div>
+        <div>{videoId}</div>
     )
     
    
